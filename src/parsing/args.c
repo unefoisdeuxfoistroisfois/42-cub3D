970d42a59f393f4e94f5babba31cea54b1bf312f
unefoisdeuxfoistroisfois <1fois2fois3fois@gmail.com>
2026-04-19 17:31:18 +0200

    Appel de fonction

--- src/parsing/args.c
+++ src/parsing/args.c
@@ -56,6 +56,8 @@ int    ft_line(char *str)
             free (line);
     }
     close (fd); 
+    ft_check_first_last_line(&maps);
+    ft_check_left_right_side(&maps);
     if (maps.no == 0 || maps.so == 0 || maps.we == 0 || maps.ea == 0 
         || maps.f == 0 || maps.c == 0)
     {
